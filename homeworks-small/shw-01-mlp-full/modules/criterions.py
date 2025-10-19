f3c950fb9089d3595251eac5221b110bf7b9bb60
xiyori <foma@shipilov.ru>
2025-10-20 01:37:53 +0300

    Add small homework 1

---
+++ homeworks-small/shw-01-mlp-full/modules/criterions.py
@@ -0,0 +1,56 @@
+import numpy as np
+from .base import Criterion
+from .activations import LogSoftmax
+
+
+class MSELoss(Criterion):
+    """
+    Mean squared error criterion
+    """
+    def compute_output(self, input: np.ndarray, target: np.ndarray) -> float:
+        """
+        :param input: array of size (batch_size, *)
+        :param target:  array of size (batch_size, *)
+        :return: loss value
+        """
+        assert input.shape == target.shape, 'input and target shapes not matching'
+        # replace with your code ｀、ヽ｀、ヽ(ノ＞＜)ノ ヽ｀☂｀、ヽ
+        return super().compute_output(input, target)
+
+    def compute_grad_input(self, input: np.ndarray, target: np.ndarray) -> np.ndarray:
+        """
+        :param input: array of size (batch_size, *)
+        :param target:  array of size (batch_size, *)
+        :return: array of size (batch_size, *)
+        """
+        assert input.shape == target.shape, 'input and target shapes not matching'
+        # replace with your code ｀、ヽ｀、ヽ(ノ＞＜)ノ ヽ｀☂｀、ヽ
+        return super().compute_grad_input(input, target)
+
+
+class CrossEntropyLoss(Criterion):
+    """
+    Cross-entropy criterion over distribution logits
+    """
+    def __init__(self, label_smoothing: float = 0.0):
+        super().__init__()
+        self.log_softmax = LogSoftmax()
+        self.label_smoothing = label_smoothing
+
+    def compute_output(self, input: np.ndarray, target: np.ndarray) -> float:
+        """
+        :param input: logits array of size (batch_size, num_classes)
+        :param target: labels array of size (batch_size, )
+        :return: loss value
+        """
+        # replace with your code ｀、ヽ｀、ヽ(ノ＞＜)ノ ヽ｀☂｀、ヽ
+        return super().compute_output(input, target)
+
+    def compute_grad_input(self, input: np.ndarray, target: np.ndarray) -> np.ndarray:
+        """
+        :param input: logits array of size (batch_size, num_classes)
+        :param target: labels array of size (batch_size, )
+        :return: array of size (batch_size, num_classes)
+        """
+        # replace with your code ｀、ヽ｀、ヽ(ノ＞＜)ノ ヽ｀☂｀、ヽ
+        return super().compute_grad_input(input, target)
